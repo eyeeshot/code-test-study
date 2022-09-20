f176af7906f7cba3bb519c8d91a371ae833a2b69
dominic <dominic@tmon.co.kr>
2022-09-20 09:19:44 +0900

    modify

--- src/HackerrankPractice/oneMonthPreparationKit/miniMaxSum.java
+++ src/HackerrankPractice/oneMonthPreparationKit/miniMaxSum.java
@@ -12,17 +12,15 @@ import static java.util.stream.Collectors.toList;
 
 public class miniMaxSum {
     public static void main(String[] args) throws IOException {
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//
-//        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-//                .map(Integer::parseInt)
-//                .collect(toList());
-//
-//        miniMaxSum(arr);
-//
-//        bufferedReader.close();
-        List<Integer> arr = Arrays.asList(256741038, 623958417, 467905213, 714532089, 938071625);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
         miniMaxSum(arr);
+
+        bufferedReader.close();
     }
 
     /*
